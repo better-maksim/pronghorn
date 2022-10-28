2b023a66ff03f4cfd6792090d44442dfef89e675
guozhu <guozhu@bianfeng.com>
2022-10-28 21:04:14 +0800

    feat(target):target 增加 name 字段

--- pkg/target/target.go
+++ pkg/target/target.go
@@ -1,13 +1,15 @@
 package target
 
 type target struct {
+	Name   string // target 名称
 	Host   string // 目标 IP 或者域名
 	Port   int    // 目标端口号
 	Weight int    // 权重
 }
 
-func NewTarget(host string, port, weight int) *target {
+func NewTarget(name, host string, port, weight int) *target {
 	return &target{
+		Name:   name,
 		Host:   host,
 		Port:   port,
 		Weight: weight,
